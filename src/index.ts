41995df21b4cbabcd61695a7bc98dedc5e74a7c0
copperdevs <github@copperdevs.com>
2025-10-27 09:12:43 -0500

    did stuff

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,24 @@ const app = new Hono()
 	.use(logger())
 	.use(prettyJSON())
 
-	.post("/api/file/upload", (c) => {
+	.post("/api/file/upload/:name", async (c) => {
+		const name = c.req.param("name");
+		const path = c.req.query("path");
+		const data = await c.req.raw.blob();
+
 		return c.json({
 			message: "File uploaded successfully",
+			name: name,
+			data: {
+				size: data.size,
+				type: data.type,
+			},
+			path: path,
 			id: randomUUIDv7(),
 		});
 	})
 	.get("/api/files/:path{.+}", (c) => {
-		let filePath = c.req.param("path")!;
+		let filePath = c.req.param("path");
 
 		if (filePath.startsWith("/")) {
 			filePath = filePath.slice(1);
