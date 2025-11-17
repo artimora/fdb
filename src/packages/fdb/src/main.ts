72ba983f9207e23265d42653b45a4a6c0d8aeb8a
copperdevs <github@copperdevs.com>
2025-11-17 09:18:54 -0600

    types

--- src/packages/fdb/src/main.ts
+++ src/packages/fdb/src/main.ts
@@ -1,4 +1,5 @@
 import type { Kysely } from "kysely";
+import type { Directory, File } from "./types";
 
 export function createFDB<DB>(db: Kysely<DB>) {
 	return new fdb<DB>(db);
@@ -6,6 +7,9 @@ export function createFDB<DB>(db: Kysely<DB>) {
 
 export class fdb<DB> {
 	private db: Kysely<DB>;
+
+	public file: File = {};
+	public directory: Directory = {};
 
 	constructor(db: Kysely<DB>) {
 		this.db = db;
---
+++ src/packages/fdb/src/types.ts
@@ -0,0 +1,22 @@
+export type File = {
+	writeAllText: (path: string, text: string) => void;
+	appendAllText: (path: string, text: string) => void;
+	readAllText: (path: string) => string;
+
+	writeAllBytes: (path: string, bytes: Uint8Array) => void;
+	appendAllBytes: (path: string, bytes: Uint8Array) => void;
+	readAllBytes: (path: string) => Uint8Array;
+
+	create: (path: string) => void;
+	exists: (path: string) => boolean;
+	copy: (originalPath: string, newPath: string) => void;
+	move: (path: string) => void;
+	delete: (path: string) => void;
+};
+
+export type Directory = {
+	create: (path: string) => void;
+	delete: (path: string) => void;
+	exists: (path: string) => boolean;
+	getFiles: (path: string) => string[]; // TODO: add filter option and subdirectories option
+};
