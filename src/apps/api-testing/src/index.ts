778e73a38cabc559c9ee1ae0417fba7b0cc11a0e
copperdevs <github@copperdevs.com>
2025-11-18 18:18:58 -0600

    html homepage

--- src/apps/api-testing/src/index.ts
+++ src/apps/api-testing/src/index.ts
@@ -3,6 +3,7 @@ import { createFDB, getProvider } from "@copperdevs/fdb";
 import { getHandler } from "@copperdevs/fdb-web";
 import { Scalar } from "@scalar/hono-api-reference";
 import { Hono } from "hono";
+import { html } from "hono/html";
 import { logger } from "hono/logger";
 import { Kysely } from "kysely";
 import { BunSqliteDialect } from "kysely-bun-sqlite";
@@ -23,7 +24,21 @@ const fileDB = getHandler(createFDB(db));
 
 const app = new Hono()
 	.use(logger())
-	.get("/", (c) => c.text("Hono!"))
+	.get("/", (c) => {
+		return c.html(html`
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<title>fdb api testing</title>
+				<link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css" />
+			</head>
+			<body>
+				<a href="http://localhost:3000/api/fdb/openapi">openapi</a>
+				<a href="http://localhost:3000/scalar">scalar</a>
+			</body>
+			</html>`);
+	})
 	.get(
 		"/scalar",
 		Scalar({
