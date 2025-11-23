176effa333268419875e1a1d6b897dfd835b5aa9
copperdevs <github@copperdevs.com>
2025-11-23 13:50:59 -0600

    packaging work

--- src/packages/fdb/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./src/errors";
-export * from "./src/main";
-export * from "./src/migrations";
-export { default as migration } from "./src/migrations";
-export * from "./src/types";
-export * from "./src/util";
