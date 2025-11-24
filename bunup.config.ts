14e4babe2c016466256947d8aeee732db010a010
copperdevs <github@copperdevs.com>
2025-11-23 22:20:46 -0600

    dropped old config

--- bunup.config.ts
+++ bunup.config.ts
@@ -4,10 +4,24 @@ export default defineWorkspace([
 	{
 		name: "@copperdevs/fdb",
 		root: "src/packages/fdb",
+		config: {
+			minify: true,
+			footer: "// built with love and caffeine by copper :3",
+			unused: {
+				level: "error",
+			},
+		},
 	},
 	{
 		name: "@copperdevs/fdb-web",
 		root: "src/packages/fdb-web",
+		config: {
+			minify: true,
+			footer: "// built with love and caffeine by copper :3",
+			unused: {
+				level: "error",
+			},
+		},
 	},
 	{
 		name: "testing",
