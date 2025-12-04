440b45658ff245d95d7718102b1790b4dcd31859
copperdevs <github@copperdevs.com>
2025-12-04 10:19:26 -0600

    sub folder

--- src/tests/core/src/directories.test.ts
+++ src/tests/core/src/directories.test.ts
@@ -1,32 +1,34 @@
 import { describe, expect, test } from "bun:test";
-import { get } from "./util";
+import { charsetPath, get } from "./util";
 
 describe("directories > root", () => {
-	test("create", async () => {
-		const { db, fdb } = await get();
+	const path = "root";
 
-		await fdb.directory.create("root");
-		expect(await fdb.directory.exists("root")).toBeTrue(); // this test relies on exists, which may be buggy?
+	test("create", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path);
+		expect(await fdb.directory.exists(path)).toBeTrue(); // this test relies on exists, which may be buggy?
 	});
 
 	test("delete", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
-		await fdb.directory.create("root"); // this test relies on create, which may be buggy?
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
 
-		await fdb.directory.delete({ path: "root", onlyOnEmpty: false });
-		expect(await fdb.directory.exists("root")).toBeFalse(); // this test relies on exists, which may be buggy?
+		await fdb.directory.delete({ path: path, onlyOnEmpty: false });
+		expect(await fdb.directory.exists(path)).toBeFalse(); // this test relies on exists, which may be buggy?
 	});
 
 	test("delete > onlyOnEmpty w/ files", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
-		await fdb.directory.create("root"); // this test relies on create, which may be buggy?
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
 
-		await fdb.file.create("root/file.test");
+		await fdb.file.create(`${path}/file.test`);
 
 		try {
-			await fdb.directory.delete({ path: "root", onlyOnEmpty: true });
+			await fdb.directory.delete({ path: path, onlyOnEmpty: true });
 		} catch (err) {
 			const error = err as Error;
 			expect(error.name).toEqual("DirectoryError");
@@ -34,27 +36,27 @@ describe("directories > root", () => {
 	});
 
 	test("exists", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
-		await fdb.directory.create("root"); // this test relies on create, which may be buggy?
-		expect(await fdb.directory.exists("root")).toBeTrue();
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
+		expect(await fdb.directory.exists(path)).toBeTrue();
 	});
 
 	test("getFiles > files", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
-		await fdb.directory.create("root"); // this test relies on create, which may be buggy?
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
 
-		await fdb.file.create("root/file-1.test");
-		await fdb.file.create("root/file-2.test");
-		await fdb.file.create("root/file-3.test");
+		await fdb.file.create(`${path}/file-1.test`);
+		await fdb.file.create(`${path}/file-2.test`);
+		await fdb.file.create(`${path}/file-3.test`);
 
-		const files = await fdb.directory.getFiles({ path: "root" });
+		const files = await fdb.directory.getFiles({ path: path });
 
 		const filesParents = files.map((t) => t.parent_folder);
 		const filesNames = files.map((t) => t.name);
 
-		const parentId = await fdb.directory.getFolderId("root");
+		const parentId = await fdb.directory.getFolderId(path);
 
 		expect(files).toHaveLength(3);
 		expect(filesParents).toEqual([parentId, parentId, parentId]);
@@ -66,11 +68,11 @@ describe("directories > root", () => {
 	});
 
 	test("getFiles > empty", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
-		await fdb.directory.create("root"); // this test relies on create, which may be buggy?
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
 
-		const files = await fdb.directory.getFiles({ path: "root" });
+		const files = await fdb.directory.getFiles({ path: path });
 
 		const filesParents = files.map((t) => t.parent_folder);
 		const filesNames = files.map((t) => t.name);
@@ -81,20 +83,20 @@ describe("directories > root", () => {
 	});
 
 	test("getFolders > folders", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
-		await fdb.directory.create("root");
+		await fdb.directory.create(path);
 
-		await fdb.directory.create("root/sub-1");
-		await fdb.directory.create("root/sub-2");
-		await fdb.directory.create("root/sub-3");
+		await fdb.directory.create(`${path}/sub-1`);
+		await fdb.directory.create(`${path}/sub-2`);
+		await fdb.directory.create(`${path}/sub-3`);
 
-		const folders = await fdb.directory.getFolders({ path: "root" });
+		const folders = await fdb.directory.getFolders({ path: path });
 
 		const foldersParents = folders.map((t) => t.parent_folder);
 		const foldersNames = folders.map((t) => t.name);
 
-		const parent = (await fdb.directory.getFolderId("root"))!;
+		const parent = (await fdb.directory.getFolderId(path))!;
 
 		expect(folders).toHaveLength(3);
 		expect(foldersParents).toEqual([parent, parent, parent]);
@@ -102,11 +104,11 @@ describe("directories > root", () => {
 	});
 
 	test("getFolders > empty", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
-		await fdb.directory.create("root");
+		await fdb.directory.create(path);
 
-		const folders = await fdb.directory.getFolders({ path: "root" });
+		const folders = await fdb.directory.getFolders({ path: path });
 
 		const foldersParents = folders.map((t) => t.parent_folder);
 		const foldersNames = folders.map((t) => t.name);
@@ -117,11 +119,127 @@ describe("directories > root", () => {
 	});
 });
 
-describe("directories > sub", () => {});
+describe("directories > sub", () => {
+	const path = `root/${charsetPath()}`;
+
+	test("create", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path);
+		expect(await fdb.directory.exists(path)).toBeTrue(); // this test relies on exists, which may be buggy?
+	});
+
+	test("delete", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
+
+		await fdb.directory.delete({ path: path, onlyOnEmpty: false });
+		expect(await fdb.directory.exists(path)).toBeFalse(); // this test relies on exists, which may be buggy?
+	});
+
+	test("delete > onlyOnEmpty w/ files", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
+
+		await fdb.file.create(`${path}/file.test`);
+
+		try {
+			await fdb.directory.delete({ path: path, onlyOnEmpty: true });
+		} catch (err) {
+			const error = err as Error;
+			expect(error.name).toEqual("DirectoryError");
+		}
+	});
+
+	test("exists", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
+		expect(await fdb.directory.exists(path)).toBeTrue();
+	});
+
+	test("getFiles > files", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
+
+		await fdb.file.create(`${path}/file-1.test`);
+		await fdb.file.create(`${path}/file-2.test`);
+		await fdb.file.create(`${path}/file-3.test`);
+
+		const files = await fdb.directory.getFiles({ path: path });
+
+		const filesParents = files.map((t) => t.parent_folder);
+		const filesNames = files.map((t) => t.name);
+
+		const parentId = await fdb.directory.getFolderId(path);
+
+		expect(files).toHaveLength(3);
+		expect(filesParents).toEqual([parentId, parentId, parentId]);
+		expect(filesNames).toEqual([
+			"file-1.test",
+			"file-2.test",
+			"file-3.test"
+		]);
+	});
+
+	test("getFiles > empty", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path); // this test relies on create, which may be buggy?
+
+		const files = await fdb.directory.getFiles({ path: path });
+
+		const filesParents = files.map((t) => t.parent_folder);
+		const filesNames = files.map((t) => t.name);
+
+		expect(files).toHaveLength(0);
+		expect(filesParents).toEqual([]);
+		expect(filesNames).toEqual([]);
+	});
+
+	test("getFolders > folders", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path);
+
+		await fdb.directory.create(`${path}/sub-1`);
+		await fdb.directory.create(`${path}/sub-2`);
+		await fdb.directory.create(`${path}/sub-3`);
+
+		const folders = await fdb.directory.getFolders({ path: path });
+
+		const foldersParents = folders.map((t) => t.parent_folder);
+		const foldersNames = folders.map((t) => t.name);
+
+		const parent = (await fdb.directory.getFolderId(path))!;
+
+		expect(folders).toHaveLength(3);
+		expect(foldersParents).toEqual([parent, parent, parent]);
+		expect(foldersNames).toEqual(["sub-1", "sub-2", "sub-3"]);
+	});
+
+	test("getFolders > empty", async () => {
+		const { fdb } = await get();
+
+		await fdb.directory.create(path);
+
+		const folders = await fdb.directory.getFolders({ path: path });
+
+		const foldersParents = folders.map((t) => t.parent_folder);
+		const foldersNames = folders.map((t) => t.name);
+
+		expect(folders).toHaveLength(0);
+		expect(foldersParents).toEqual([]);
+		expect(foldersNames).toEqual([]);
+	});
+});
 
 describe("directories > undefined", () => {
 	test("create", async () => {
-		const { db, fdb } = await get();
+		const { fdb } = await get();
 
 		// there has got to be a better way to do this
 		try {
