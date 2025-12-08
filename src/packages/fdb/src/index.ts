1efe2ccd3d05aedda3b454aa7e65316b50804cd3
copperdevs <github@copperdevs.com>
2025-12-08 10:45:15 -0600

    update whats exported for fdb and ukv

--- src/packages/fdb/src/index.ts
+++ src/packages/fdb/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./errors";
 export * from "./main";
 export * from "./migrations";
-export * from "./migrations";
 export * from "./types";
 export * from "./util";
--- src/packages/ukv/src/index.ts
+++ src/packages/ukv/src/index.ts
@@ -1,4 +1,2 @@
 export * from "./main";
 export * from "./migration";
-export * from "./types";
-export * from "./operations";
